f2cbc9871a97ef22e5c57bec98f098bbd5857815
Durgesh Ramesh Hyalij <hyalijdurgesh102@gmail.com>
2026-07-23 16:15:08 +0530

    understanding normal css and folder structure for it

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/header'
+import Button from './components/button/button'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ 05-css/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div >
+      <h1 className={styles.hdr} >Hello THis is navbar yar</h1>
+    </div>
+  )
+}
+
+export default Header
