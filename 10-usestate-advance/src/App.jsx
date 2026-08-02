4ed840643cdaf9003aade0c47fe2f566f7bfedc2
Durgesh Ramesh Hyalij <hyalijdurgesh102@gmail.com>
2026-08-02 14:12:19 +0530

    here form submit change successfully is printed for milli seconds and full page reloads so to avoid it we earn something futher

--- 10-usestate-advance/src/App.jsx
+++ 10-usestate-advance/src/App.jsx
@@ -3,18 +3,17 @@ import { useState } from 'react'
 
 const App = () => {
   
-  const [num, setnum] = useState([10, 20, 30])
-
-  function numchange(){
-    const newnum = [...num]
-    newnum.push(50)
-    setnum(newnum)
+  const change = () => {
+    console.log("Form Sumitted successfully ");
+    
   }
 
   return (
     <div>
-      <h1>Hello World {num}</h1>
-      <button onClick={numchange}>Click Me</button>
+      <form onSubmit={change}>
+        <input type='text' placeholder='Enter your Name'/>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
